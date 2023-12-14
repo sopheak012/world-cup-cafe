394cb8d2ac8ece083ac0dac54d7adb211e9c0135
sopheak012 <36690234+sopheak012@users.noreply.github.com>
2023-12-14 12:23:46 -0800

    about page

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import pizzaMain from "../images/about/pizza-main.png";
+import topBorder from "../images/about/top-border.png";
+import btmBorder from "../images/about/btm-border.png";
+
+const About = () => (
+  <section id="about">
+    <div className="w-full h-full">
+      <img src={topBorder} alt="" className="w-full" />
+      <div className="bg-[#222] py-4 md:py-0">
+        <motion.h2
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true, delay: 0.5 }}
+        >
+          Our Journey
+        </motion.h2>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.75 }}
+          viewport={{ once: true }}
+        >
+          <div className="max-w-screen-lg w-full grid px-4 sm:grid-cols-2 gap-8 mx-auto text-white">
+            <div className="flex justify-center items-center">
+              <img
+                src={pizzaMain}
+                alt="The founders of Tokio Ramen"
+                className="rounded drop-shadow-lg"
+              />
+            </div>
+            <div>
+              <p className="pb-2">
+                In 2009, after immigrating from Cambodia, our founders worked
+                tirelessly to build up enough savings to realize their dream of
+                opening a restaurant. Their dedication and hard work laid the
+                foundation for what would become World Cup Cafe.
+              </p>
+              <p className="py-2">
+                Today, World Cup Cafe stands as a testament to their journey and
+                passion for providing a taste of authenticity. Our menu offers a
+                diverse range of options, including mouthwatering pizzas,
+                flavorful sandwiches, and refreshing smoothies. Each dish is
+                crafted with care and reflects the dedication to quality that
+                defines our establishment.
+              </p>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+      <img src={btmBorder} alt="" className="w-full" />
+    </div>
+  </section>
+);
+
+export default About;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import Hero from "./Hero";
+import About from "./About";
+import { motion } from "framer-motion";
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <>
       <Header />
       <Hero />
+      <motion.main
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1, delay: 0.75 }}
+        viewport={{ once: true }}
+      >
+        {children}
+      </motion.main>
+      <About />
     </>
   );
 };
